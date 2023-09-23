ac626aeec580ab19998969ff95ca135bbcae89d2
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-09-23 14:47:22 -0700

    feat: add progress view + button active status

--- script.js
+++ script.js
@@ -1,85 +1,104 @@
 var pomodoro = {
-    started : false,
-    minutes : 0,
-    seconds : 0,
-    fillerHeight : 0,
-    fillerIncrement : 0,
-    interval : null,
-    minutesDom : null,
-    secondsDom : null,
-    fillerDom : null,
-    init : function(){
-      var self = this;
-      this.minutesDom = document.querySelector('#minutes');
-      this.secondsDom = document.querySelector('#seconds');
-      this.fillerDom = document.querySelector('#filler');
-      this.interval = setInterval(function(){
-        self.intervalCallback.apply(self);
-      }, 1000);
-      document.querySelector('#work').onclick = function(){
-        self.startWork.apply(self);
-      };
-      document.querySelector('#shortBreak').onclick = function(){
-        self.startShortBreak.apply(self);
-      };
-      document.querySelector('#longBreak').onclick = function(){
-        self.startLongBreak.apply(self);
-      };
-      document.querySelector('#stop').onclick = function(){
-        self.stopTimer.apply(self);
-      };
-    },
-    resetVariables : function(mins, secs, started){
-      this.minutes = mins;
-      this.seconds = secs;
-      this.started = started;
-      this.fillerIncrement = 200/(this.minutes*60);
-      this.fillerHeight = 0;  
-    },
-    startWork: function() {
-      this.resetVariables(25, 0, true);
-    },
-    startShortBreak : function(){
-      this.resetVariables(5, 0, true);
-    },
-    startLongBreak : function(){
-      this.resetVariables(15, 0, true);
-    },
-    stopTimer : function(){
-      this.resetVariables(25, 0, false);
-      this.updateDom();
-    },
-    toDoubleDigit : function(num){
-      if(num < 10) {
-        return "0" + parseInt(num, 10);
-      }
-      return num;
-    },
-    updateDom : function(){
-      this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
-      this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
-      this.fillerHeight = this.fillerHeight + this.fillerIncrement;
-      this.fillerDom.style.height = this.fillerHeight + 'px';
-    },
-    intervalCallback : function(){
-      if(!this.started) return false;
-      if(this.seconds == 0) {
-        if(this.minutes == 0) {
-          this.timerComplete();
-          return;
-        }
-        this.seconds = 59;
-        this.minutes--;
-      } else {
-        this.seconds--;
-      }
-      this.updateDom();
-    },
-    timerComplete : function(){
-      this.started = false;
-      this.fillerHeight = 0;
+  started: false,
+  goalTime: 0,
+  minutes: 0,
+  seconds: 0,
+  width: 0,
+  fillerIncrement: 0,
+  interval: null,
+  minutesDom: null,
+  secondsDom: null,
+  fillerDom: null,
+  browserWidth: window.visualViewport.width,
+  elapsedTime: 0,
+  init: function () {
+    var self = this;
+    this.minutesDom = document.querySelector('#minutes');
+    this.secondsDom = document.querySelector('#seconds');
+    this.fillerDom = document.querySelector('#filler');
+    this.fillerDom.style.width = '0px';
+    this.interval = setInterval(function () {
+      self.intervalCallback.apply(self);
+    }, 1000);
+    document.querySelector('#work').onclick = function () {
+      self.startWork.apply(self);
+    };
+    document.querySelector('#shortBreak').onclick = function () {
+      self.startShortBreak.apply(self);
+    };
+    document.querySelector('#longBreak').onclick = function () {
+      self.startLongBreak.apply(self);
+    };
+    document.querySelector('#reset').onclick = function () {
+      self.resetTimer.apply(self);
+    };
+    window.visualViewport.addEventListener('resize', function () {
+      self.browserWidth = window.visualViewport.width;
+    });
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        const btns = document.querySelectorAll('.button');
+        btns.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+        e.target.classList.add('active')
+      });
+    });
+  },
+  resetVariables: function (mins, secs, started) {
+    this.elapsedTime = 0;
+    this.minutes = mins;
+    this.seconds = secs;
+    this.started = started;
+    this.width = 0;
+    this.goalTime = mins * 60 + secs;
+  },
+  startWork: function () {
+    this.resetVariables(25, 0, true);
+  },
+  startShortBreak: function () {
+    this.resetVariables(5, 0, true);
+  },
+  startLongBreak: function () {
+    this.resetVariables(15, 0, true);
+  },
+  resetTimer: function () {
+    this.resetVariables(25, 0, false);
+    this.updateDom();
+  },
+  toDoubleDigit: function (num) {
+    if (num < 10) {
+      return "0" + parseInt(num, 10);
     }
+    return num;
+  },
+  updateDom: function () {
+    this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+    this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+    this.elapsedTime++;
+    this.width = (this.elapsedTime / this.goalTime) * this.browserWidth;
+    this.fillerDom.style.width = this.width + 'px';
+  },
+  intervalCallback: function () {
+    if (!this.started) return false;
+    if (this.seconds == 0) {
+      if (this.minutes == 0) {
+        this.timerComplete();
+        return;
+      }
+      this.seconds = 59;
+      this.minutes--;
+    } else {
+      this.seconds--;
+    }
+    this.updateDom();
+  },
+  timerComplete: function () {
+    this.started = false;
+    this.width = 0;
+  }
 };
-window.onload = function(){
+window.onload = function () {
   pomodoro.init();
 };
