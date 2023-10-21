3684c22c2a2eb8340de5558c188dd5b5ddccc4a9
Lukepark1 <hg7377@gmail.com>
2023-10-21 13:32:58 -0700

    Luke edit.v1

--- script/pomodoro.mjs
+++ script/pomodoro.mjs
@@ -112,3 +112,14 @@ class Pomodoro {
 }
 
 export default Pomodoro;
+
+function randombg(){
+  var random= Math.floor(Math.random() * 6) + 0;
+  var bigSize = ["url('http://placehold.it/300&text=banner1')",
+                 "url('http://placehold.it/300&text=banner2')",
+                 "url('http://placehold.it/300&text=banner3')",
+                 "url('http://placehold.it/300&text=banner4')",
+                 "url('http://placehold.it/300&text=banner5')",
+                 "url('http://placehold.it/300&text=banner6')"];
+  document.getElementById("random").style.backgroundImage=bigSize[random];
+}
